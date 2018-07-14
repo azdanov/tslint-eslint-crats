e78607d728eeca3c98a63cdb3a184ca502738b0c
Anton Zdanov <azdanov@protonmail.com>
2018-07-14 09:17:18 +0300

    chore: revert to npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import './App.css';
-import logoSvg from './logo.svg';
+import * as React from "react";
+import "./App.css";
+import logoSvg from "./logo.svg";
 
 class App extends React.Component {
   public render() {
